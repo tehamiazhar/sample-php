5feedf93710a386ecbef00adf6c0e8b70936da9b
Developer <developer@Admins-Mac-mini.local>
2019-12-16 17:31:20 +0500

    initial commit

---
+++ sample-php.php
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue255;\red0\green0\blue173;
+\red13\green103\blue1;\red210\green0\blue5;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c73333;
+\cssrgb\c0\c46667\c0;\cssrgb\c86667\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl320\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <html>\
+\'a0<head>\
+\'a0\'a0<title>PHP\'a0Test</title>\
+\'a0</head>\
+\'a0<body>\
+\'a0\cf4 \strokec4 <?php\'a0\cf5 \strokec5 echo\'a0\cf6 \strokec6 '<p>Hello\'a0World</p>'\cf5 \strokec5 ;\'a0\cf4 \strokec4 ?>\cf2 \strokec2 \'a0\
+\'a0</body>\
+</html>}
